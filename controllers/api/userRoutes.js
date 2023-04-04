dee06e3d2394d1f2f73a6044cb5e18ff3e2b6eeb
Harrison <harrylamm@icloud.com>
2023-04-04 10:29:32 -0700

    added login logout routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { User, Article } = require("../../models/");
 
-// login routes
+// login page route
 router.get("/login", async (req, res) => {
   try {
     res.render("login");
@@ -11,17 +11,32 @@ router.get("/login", async (req, res) => {
   }
 });
 
+// user login route
 router.post("/login", async (req, res) => {
   try {
-    const userData = await User.create({
-      name: req.body.name,
-      password: req.body.password,
+    const userData = await User.findOne({
+      where: {
+        name: req.body.username,
+      },
     });
 
-    req.session.save(() => {
-      req.session.loggedIn = true;
+    if (!userData) {
+      res.status(400).json({ message: "Incorrect email or password" });
+      return;
+    }
 
-      res.status(200).json(userData);
+    const validPw = await userData.checkPassword(req.body.password);
+
+    if (!validPw) {
+      res.status(400).json({ message: "Incorrect email or password" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json({ user: userData, message: "You are now logged in!" });
     });
   } catch (error) {
     console.log(error);
@@ -29,12 +44,18 @@ router.post("/login", async (req, res) => {
   }
 });
 
-// logout routes
-router.post('/logout', async (req, res) => {
+// user logout route
+router.post("/logout", async (req, res) => {
   try {
-    
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
   }
-})
+});
